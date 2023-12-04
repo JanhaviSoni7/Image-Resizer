549550826f3b00c6fc36675d6885cef093cabfb3
JanhaviSoni7 <Janhavissoni@gmail.com>
2023-12-04 20:49:23 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+
+source = "download.png"
+destination = "newimage.png"
+scale_percent = 50
+
+src = cv2.imread(source , cv2.IMREAD_UNCHANGED)
+# cv2.imshow("title", image)
+
+new_width = int(src.shape[1] * scale_percent / 100)
+new_height = int(src.shape[0] * scale_percent / 100)
+
+output = cv2.resize(src, (new_width, new_height))
+
+cv2.imwrite(destination,output)
+#cv2.waitKey(0)
